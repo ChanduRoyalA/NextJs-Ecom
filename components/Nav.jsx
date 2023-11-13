c793e6d6525ea310ec157179e08d21195f33627b
ChanduRoyalA <chandua.royal@gmail.com>
2023-11-13 17:30:21 +0530

    update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri"
 import { AiOutlineUser } from 'react-icons/ai'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 
 
 const Nav = () => {
@@ -26,26 +26,31 @@ const Nav = () => {
               <p className='hover:border-b-2 hover:border-b-[#214E34] border-2 border-white rounded-none'>Liked</p>
           </div>
       <button className='hidden md:flex bg-[#214E34] text-white font-semibold px-4 py-2' onClick={handleLogout}>Logout</button>
-      <button className='md:hidden' onClick={()=>{setmbView(!mbView)}} >{mbView ? <RiCloseLine style={{ fontSize: '25px', color: '#214E34' }} /> : <RiMenu3Line style={{ fontSize: '25px', color: '#214E34' }} />}</button>
+      <button className='md:hidden' onClick={() => { setmbView(!mbView) }} ><RiMenu3Line style={{ fontSize: '25px', color: '#214E34' }} /> </button>
+      {/* <RiCloseLine style={{ fontSize: '25px', color: '#214E34' }} /> */}
       {
         mbView && (
           <motion.div
-            initial={{ width: 0 }}
-            animate={{width:'70vw'}}
-            className='md:hidden pt-2 bg-white h-fit absolute w-[70vw] top-10 rounded-lg'>
-            <Image src={Logo} width={90} />
-            <div className='flex flex-col justify-between'>
-              <div className='px-10 py-12  flex justify-between flex-col h-[40vh] text-[#214E34]'>
-                <p className='py-2 px-5 text-lg flex gap-2 items-center hover:bg-[#214E34] hover:text-white rounded-md'><AiOutlineUser style={{ color: "white" }} /> Profile</p>
+            initial={{ x:-200  }}
+            animate={{ x:0 }}
+            className='md:hidden w-screen pt-2 px-16 bg-white h-screen absolute  top-10 left-0 rounded-lg'>
+            <div className='flex justify-between' >
+              <Image src={Logo} width={90} />
+              <button className='md:hidden' onClick={() => { setmbView(!mbView) }} ><RiCloseLine style={{ fontSize: '25px', color: '#214E34' }} /></button>
+
+            </div>
+            <div className='flex flex-col justify-between items-start'>
+              <div className='py-12  flex justify-between flex-col h-[40vh] text-[#214E34] w-full'>
+                <p className='py-2  px-5 text-lg flex gap-2 items-center hover:bg-[#214E34] hover:text-white rounded-md'><AiOutlineUser style={{ color: "white" }} /> Profile</p>
                 <p className='py-2 px-5 text-lg flex gap-2 items-center hover:bg-[#214E34] hover:text-white rounded-md'><AiOutlineUser style={{ color: "white" }} /> Profile</p>
                 <p className='py-2 px-5 text-lg flex gap-2 items-center hover:bg-[#214E34] hover:text-white rounded-md'><AiOutlineUser style={{ color: "white" }} /> Profile</p>
                 <p className='py-2 px-5 text-lg flex gap-2 items-center hover:bg-[#214E34] hover:text-white rounded-md'><AiOutlineUser style={{ color: "white" }} /> Profile</p>
               </div>
-              <div className='px-10 py-12'>
-                <button className=' self-start bg-[#214E34] text-white font-semibold px-6 rounded-md py-2' onClick={handleLogout}>Logout</button>
-              </div>
+              
+                <button className='w-full bg-[#214E34] text-white font-semibold px-6 rounded-md py-2' onClick={handleLogout}>Logout</button>
+         
           </div>
-          </motion.div>
+            </motion.div>
         )
       }
     </nav>
